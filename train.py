6f6fbd93efad523db697e4798500867dcb29d1ae
WeifaGan <44939038+WeifaGan@users.noreply.github.com>
2020-10-29 13:50:38 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from models import Resnet
 import torch.optim as optim
 import torch.nn as nn
 from torch.utils.data import sampler
+from utils.draw import result_visual
 
 parser = argparse.ArgumentParser(description="argument of training")
 parser.add_argument('--lr',default=0.001,type=float,help="learning rate")
@@ -14,7 +15,6 @@ parser.add_argument("--resume",'-r',action='store_true',help="resume from cheakp
 args = parser.parse_args()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu' 
-
 
 print("==>Preparing data")
 
@@ -36,11 +36,11 @@ trainset = torchvision.datasets.CIFAR10(
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=False, num_workers=2)
 
-valset = torchvision.datasets.CIFAR10(
+testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_val)
 valloader = torch.utils.data.DataLoader(
-    valset, batch_size=100, shuffle=False, num_workers=2,
-    sampler=sampler.SubsetRandomSampler(0,1000)) 
+    testset, batch_size=100, shuffle=False, num_workers=2,
+    sampler=sampler.SubsetRandomSampler(range(0.2*len(testset)))) 
 
 
 print("==>Building model")
@@ -59,7 +59,8 @@ if args.resume:
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(),lr=args.lr,momentum=0.9,weight_decay=5e-4) 
 
-train_loss,correct,total = 0,0,0
+train_loss,correct,total,val_loss = 0,0,0,0
+train_loss_dw,val_loss_dw,train_acc_dw, val_acc_dw = [], [],[],[]
 
 for epoch in range(100):
     for batch_idx,(inputs,target) in enumerate(trainloader):
@@ -76,33 +77,38 @@ for epoch in range(100):
         correct     = predicted.eq(target).sum().item()
 
         if batch_idx %10 ==0:
-            val_loss_v = 0
             for batch_idx_v,(inputs_v,target_v) in enumerate(valloader):
                 inputs_v,targets_v = inputs.to(device),target.to(device)
                 outputs_v = net(inputs_v)
                 loss_v = criterion(outputs_v,targets_v)
 
-                val_loss_v      += loss_v.item()
+                val_loss      += loss_v.item()
                 _,predicted_v = outputs_v.max(1)
                 total_v       = target_v.size(0)
                 correct_v     = predicted_v.eq(target_v).sum().item()
 
+                
+                train_loss_dw.append(train_loss/(batch_idx+1))
+                val_loss_dw.append(val_loss/(batch_idx+1))
+
+
             print("train_loss:%.3f|train_acc:%.3f|val_loss:%.3f|val_acc:%.3f"%(train_loss/(batch_idx+1),\
-                (100.*correct/total),val_loss_v/(batch_idx_v+1),(100.*correct_v/total_v)))
+                (100.*correct/total),val_loss/(batch_idx_v+1),(100.*correct_v/total_v)))
         
         val_acc = 100.*correct_v/total_v
         if val_acc>best_acc:
-            print("saving")
+            print("saving...")
             state = {
                 'net': net.state_dict(),
                 'acc':val_acc,
                 'epoch':epoch,
             }
-            info = str(epoch)+'_'+str(batch_idx)
             if not os.path.isdir('checkpoint'):
                 os.mkdir('checkpoint')
-            torch.save(state,'./checkpoint/'+info+'_ckpt.pth')
+            torch.save(state,'./checkpoint/ckpt.pth')
             best_acc = val_acc
+        
+result_visual(train_loss_dw,val_loss_dw,train_acc_dw,val_acc_dw)
 
 
 
